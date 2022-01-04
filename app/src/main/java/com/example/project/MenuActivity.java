88573aecfb89af197468cf1f053fdcfe46ab71ef
Roma <liv@mail.ru>
2022-01-04 12:26:43 +0800

    12:26

--- app/src/main/java/com/example/project/MenuActivity.java
+++ app/src/main/java/com/example/project/MenuActivity.java
@@ -11,25 +11,13 @@ import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
-    ImageButton firstBtn;
 
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        firstBtn = findViewById(R.id.firstButn);
 
-//        firstBtn.setImageResource(R.drawable.irkutsk);
-
-        firstBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "trituriturit", Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
     }
