8b4343e20d4be65d8d59f445c23247990e3c6840
Roma <liv@mail.ru>
2022-01-07 20:30:57 +0800

    20:30

---
+++ app/src/main/java/com/example/project/LevelOne.java
@@ -0,0 +1,152 @@
+package com.example.project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class LevelOne extends AppCompatActivity {
+
+    TextView levelOneQuestion;
+    Button levelOneAnswerOne;
+    Button levelOneAnswerTwo;
+    Button levelOneAnswerThree;
+    Button levelOneAnswerFour;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef = db.collection("questionFirstLevel").document("questionFirstLevel");
+    private static final String TAG = "USER";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_one);
+
+        levelOneQuestion = findViewById(R.id.levelOneQuestion);
+
+        levelOneAnswerOne = findViewById(R.id.levelOneAnswerOne);
+        levelOneAnswerTwo = findViewById(R.id.levelOneAnswerTwo);
+        levelOneAnswerThree = findViewById(R.id.levelOneAnswerThree);
+        levelOneAnswerFour = findViewById(R.id.levelOneAnswerFour);
+
+
+
+
+
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        levelOneQuestion.setText(document.getData().get("questionFirstLevel").toString());
+
+                        levelOneAnswerOne.setText(document.getData().get("answerFirstLevelOne").toString());
+                        levelOneAnswerTwo.setText(document.getData().get("answerFirstLevelTwo").toString());
+                        levelOneAnswerThree.setText(document.getData().get("answerFirstLevelThree").toString());
+                        levelOneAnswerFour.setText(document.getData().get("answerFirstLevelFour").toString());
+
+
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+        levelOneAnswerOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(LevelOne.this);
+                builder1.setMessage("Ответ неверный");
+                builder1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder1.create();
+                alertDialog.show();
+
+            }
+        });
+        levelOneAnswerTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(LevelOne.this);
+                builder1.setMessage("Ответ неверный");
+                builder1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder1.create();
+                alertDialog.show();
+
+            }
+        });
+        levelOneAnswerThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(LevelOne.this);
+                builder1.setMessage("Ответ неверный");
+                builder1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder1.create();
+                alertDialog.show();
+
+            }
+        });
+
+        levelOneAnswerFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(LevelOne.this);
+                builder1.setMessage("Верно!!!");
+                builder1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(LevelOne.this,MainActivity.class);
+                        startActivity(intent);
+                        LevelOne.this.finish();
+
+                    }
+                });
+                AlertDialog alertDialog = builder1.create();
+                alertDialog.show();
+
+            }
+        });
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -31,69 +31,61 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button firstBtn;
-    Button secondBtn;
-    Button thirdBtn;
-
-
-    EditText editText;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstBtn = findViewById(R.id.firstBtn);
-        secondBtn = findViewById(R.id.secondBtn);
-        thirdBtn = findViewById(R.id.thirdBtn);
-
-        editText = findViewById(R.id.exampletextEdit);
-
-
-
-
-        firstBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder bui1 = new AlertDialog.Builder(MainActivity.this);
-                bui1.setMessage("Ответ неверный");
-
-                bui1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-                AlertDialog alei = bui1.create();
-                alei.show();
-
-            }
-        });
-
-        secondBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder bui2 = new AlertDialog.Builder(MainActivity.this);
-                bui2.setMessage("Молодец");
-                bui2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-                        startActivity(intent);
-                        MainActivity.this.finish();
-
-                    }
-                });
-                AlertDialog alei2 = bui2.show();
-                alei2.show();
-            }
-        });
-
-
-
 
 
     }
 
+    public void levelFirstBtn(View view) {
+        Intent intent = new Intent(MainActivity.this,LevelOne.class);
+        startActivity(intent);
+        MainActivity.this.onPause();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        firstBtn.setOnClickListener(new View.OnClickListener() {
+//@Override
+//public void onClick(View view) {
+//        AlertDialog.Builder bui1 = new AlertDialog.Builder(MainActivity.this);
+//        bui1.setMessage("Ответ неверный");
+//
+//        bui1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//@Override
+//public void onClick(DialogInterface dialogInterface, int i) {
+//        dialogInterface.cancel();
+//        }
+//        });
+//        AlertDialog alei = bui1.create();
+//        alei.show();
+//
+//        }
+//        });
